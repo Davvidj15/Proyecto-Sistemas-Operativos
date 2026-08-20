f6fff4ed3419ffd4720b9ad49ea30f1578160324
Nacht56 <nachoutu06@gmail.com>
2026-08-20 14:15:58 -0300

    Fix directory creation and deletion messages

--- Opcion1.sh
+++ Opcion1.sh
@@ -1,21 +1,22 @@
 echo "¿Desea crear un directorio? (Si o No)"
 read vRespuesta
-if ($vRespuesta)
+if test $vRespuesta == "Si" -o $vRespuesta == "si"
 then
-	echo "Ingrese el nombre que desea poner al directorio:"
-	read nomDir
-	if mkdir $nomDir 2> /dev/null
-	then
-		echo "El directorio ha sido creado exitosamente"
-	else
-		echo "El directorio ya existe"
+        echo "Ingrese el nombre que desea poner al directorio:"
+        read nomDir
+        if mkdir $nomDir 2> /dev/null
+        then
+                echo "El directorio $nomDir ha sido creado exitosamente"
+        else
+                echo "El directorio $nomDir ya existe"
+        fi
 else
-	echo "¿Qué directorio desea eliminar?"
-	read eliDir
-	if rmdir $eliDir 2> /dev/null
-	then
-		echo "El diretorio fue eliminado exitosamente"
-	else
-		echo "El directorio no ha sido eliminado"
-		echo "El error puede deberse a que el directorio $eliDir tiene algún archivo dentro"
+        echo "¿Qué directorio desea eliminar?"
+        read eliDir
+        if rm -r $eliDir 2> /dev/null
+        then
+                echo "El diretorio $eliDir fue eliminado exitosamente"
+        else
+                echo "El directorio $eliDir no ha sido eliminado"
+        fi
 fi
